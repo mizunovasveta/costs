b54bdd9658d321ccff0e434d99c94949ded19db8
mizunovasveta <mizunovasveta@gmail.com>
2024-03-12 22:17:09 +0400

    created updates

--- polls/models.py
+++ polls/models.py
@@ -2,26 +2,23 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-
-    # CATEGORY_CHOICES = [
-    #     ('markets', 'Markets'),
-    #     ('pharmacy', 'Pharmacy'),
-    #     ('entertainment', 'Entertainment'),
-    #     ('bookstore', 'Bookstore'),
-    #     ('electronics and gadgets', 'Electronics and gadgets'),
-    #     ('appliances', 'Appliances'),
-    #     ('beauty', 'Beauty'),
-    #     ('wellness', 'Wellness')
-    # ]
     def __str__(self):
         return self.name
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=3, default='USD')
+    def __str__(self):
+        return self.name
+
+
 
 class Expense(models.Model):
     name = models.CharField(max_length=200)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     pub_date = models.DateField("date published")
     amount = models.FloatField(default=0.00)
-    currency = models.CharField(max_length=3, default='USD')
+    currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
